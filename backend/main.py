8e7d8ccecb40f1b7aeb6cb58e4efd4b3d7cbf1b1
Jay patel <jayspatel190506@gmail.com>
2026-03-14 18:25:52 +0530

    all urls updates

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,30 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
+import os
 
-from app import preprocess_img, get_embeddings, get_min_similarity # use new match function
+celeb_image_urls = {
+    "Angelina Jolie": "https://upload.wikimedia.org/wikipedia/commons/a/ad/Angelina_Jolie_2_June_2014_%28cropped%29.jpg",
+    "Brad Pitt": "https://upload.wikimedia.org/wikipedia/commons/9/90/Brad_Pitt-69858.jpg",
+    "Denzel Washington": "https://upload.wikimedia.org/wikipedia/commons/a/a4/Denzel_Washington_cropped_02_b.jpg",
+    "Hugh Jackman": "https://upload.wikimedia.org/wikipedia/commons/6/65/Hugh_Jackman_%288302902601%29.jpg",
+    "Jennifer Lawrence": "https://upload.wikimedia.org/wikipedia/commons/5/56/Jennifer_Lawrence_83rd_Oscars.jpg",
+    "Johnny Depp": "https://upload.wikimedia.org/wikipedia/commons/7/79/Johnny_Depp_Deauville_2019.jpg",
+    "Kate Winslet": "https://upload.wikimedia.org/wikipedia/commons/9/99/Kate_Winslet_at_The_Dressmaker_event_TIFF_%28headshot%29.jpg",
+    "Leonardo DiCaprio": "https://upload.wikimedia.org/wikipedia/commons/8/8f/LeonardoDiCaprioNov08.jpg",
+    "Megan Fox": "https://upload.wikimedia.org/wikipedia/commons/e/e9/Megan_Fox_2014.jpg",
+    "Natalie Portman": "https://upload.wikimedia.org/wikipedia/commons/d/d3/Natalie_Portman_%2848470988352%29_%28cropped%29.jpg",
+    "Nicole Kidman": "https://upload.wikimedia.org/wikipedia/commons/2/28/Nicole_Kidman_Cannes_2017_2.jpg",
+    "Robert Downey Jr": "https://upload.wikimedia.org/wikipedia/commons/d/d3/Robert_Downey%2C_Jr._2012.jpg",
+    "Sandra Bullock": "https://upload.wikimedia.org/wikipedia/commons/1/15/Sandra_Bullock_in_July_2013.jpg",
+    "Scarlett Johansson": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Scarlett_Johansson_in_Kuwait_01b-tweaked.jpg",
+    "Tom Cruise": "https://upload.wikimedia.org/wikipedia/commons/1/11/Tom_Cruise_May_2022_%28cropped%29.jpg",
+    "Tom Hanks": "https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Hanks_face.jpg",
+    "Will Smith": "https://upload.wikimedia.org/wikipedia/commons/8/84/Will-smith-userbox.jpg?_=20111130192147",
+}
+
+
+from app import preprocess_img, get_embeddings, get_min_similarity 
 
 app = FastAPI()
 
@@ -33,8 +55,12 @@ async def findmatch(photo: UploadFile = File(...)):
 
         celeb_name = get_min_similarity(emb)
 
+        
+
+
         return {
-            "matched": celeb_name
+            "matched": celeb_name,
+            "matched_img_url":celeb_image_urls[celeb_name]
         }
 
     except Exception as e:
@@ -42,3 +68,5 @@ async def findmatch(photo: UploadFile = File(...)):
             "error": "Something went wrong",
             "details": str(e)
         }
+
+
